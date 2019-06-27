d0cb93c2a3a053f8fef343b1e1987bd432054aef
liuhanru <m18401773135@163.com>
2019-06-27 18:05:48 +0800

    修改提交6.05

--- src/main/java/com/shenzhen/teamway/assessctloffivesys/AssessctloffivesysApplication.java
+++ src/main/java/com/shenzhen/teamway/assessctloffivesys/AssessctloffivesysApplication.java
@@ -16,6 +16,7 @@ public class AssessctloffivesysApplication implements CommandLineRunner {
     private MqttFutureComsumer mqttFutureComsumer;
 
     public static void main(String[] args) {
+
         SpringApplication.run(AssessctloffivesysApplication.class, args);
     }
 
